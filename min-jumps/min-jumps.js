bab5d9723bbf809cae4f50a928f1116c07468cdd
Angela Vuong <vuong.angela@gmail.com>
2020-02-19 18:00:11 -0500

    up to jump problem

--- min-jumps/min-jumps.js
+++ min-jumps/min-jumps.js
@@ -2,7 +2,22 @@
 
 // Complete this algo
 const minJumps = arr => {
+  // start at the last indext of the array.
+  let currentFinishIndex = arr.length - 1;
 
+  // Loop through array and identify the lowest index that is the number that crossed the finish line. Set the new finish line.
+  let jumps = 0;
+  while (currentFinishIndex > 0) {
+    for (let i = 0; i < arr.length - 1; i++) {
+      if (arr[i] > currentFinishIndex - i) {
+        currentFinishIndex = i;
+        jumps++;
+      }
+    }
+  }
+  return jumps;
+
+  // repeat until the identified number is at array[0].
 };
 
-module.exports = minJumps
+module.exports = minJumps;
