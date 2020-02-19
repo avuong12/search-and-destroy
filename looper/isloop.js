91cbaa416bdc80f6d0739c5840de263d02cefd77
Angela Vuong <vuong.angela@gmail.com>
2020-02-19 17:13:04 -0500

    completed isLoop for linkedList

--- looper/isloop.js
+++ looper/isloop.js
@@ -1,10 +1,21 @@
 'use strict';
 
 //Complete this algo
-const isLoop = (linkedlist) => {
-
+const isLoop = linkedList => {
+  let nodeObj = {};
+  // establish starting point.
+  let currentNode = linkedList.head;
+  while (currentNode.next !== null) {
+    if ([currentNode.value] in nodeObj) {
+      return true;
+    } else {
+      nodeObj[currentNode.value] = true;
+      // assign the current to be the next node.
+      currentNode = currentNode.next;
+    }
+  }
+  return false;
 };
-
 
 /*
 EXTRA CREDIT:
@@ -14,4 +25,4 @@ This function should return the Node value the loop begins at
 Remember to write some test specs too!
 
 */
-module.exports = isLoop
+module.exports = isLoop;
